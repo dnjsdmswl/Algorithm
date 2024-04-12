3474c5a33cd77634c27421b2cf8aa6db49692354
은지 <128462853+dnjsdmswl@users.noreply.github.com>
2024-04-12 23:51:50 +0900

    [level 0] Title: a와 b 출력하기, Time: 1.64 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181951. a와 b 출력하기/a와 b 출력하기.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int a;
+    int b;
+    scanf("%d %d", &a, &b);
+    printf("a = ");
+    printf("%d\n", a);
+    printf("b = ");
+    printf("%d", b);
+    return 0;
+}
