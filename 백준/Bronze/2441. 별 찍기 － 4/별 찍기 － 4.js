1426c03435d7d7bff6e149727e75457b7331ea99
은지 <128462853+dnjsdmswl@users.noreply.github.com>
2025-02-09 22:45:32 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 96 ms, Memory: 9520 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.js
@@ -0,0 +1,5 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync("/dev/stdin").toString().trim());
+for (let i = input; i >= 1; i--) {
+  console.log(" ".repeat(input - i) + "*".repeat(i));
+}
